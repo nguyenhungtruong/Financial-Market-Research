f379b5e1c50831292bdc2e41ab54db3b4cad77cd
nguyenhungtruong <53572063+nguyenhungtruong@users.noreply.github.com>
2023-02-22 08:58:53 +0700

    Add files via upload

---
+++ knn_select stock_1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 18 19:04:38 2023
+KNN Algorithm_ Predict  whether stocks will be Supper Stock
+@author: Nguyen_Hung_Truong
+"""
+conda install spyder-kernels scikit-learn -y
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
